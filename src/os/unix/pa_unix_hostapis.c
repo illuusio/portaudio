853ddf5799fe3048e73732226d0e8969dd200dc8
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2015-11-17 09:06:39 +0200

    PulseAudio: Add Skeleton HostAPI Pulseaudio-driver to Portaudio.
    More information About Pulseaudio visit: https://www.freedesktop.org/wiki/Software/PulseAudio/

--- src/os/unix/pa_unix_hostapis.c
+++ src/os/unix/pa_unix_hostapis.c
@@ -43,6 +43,7 @@
 #include "pa_hostapi.h"
 
 PaError PaJack_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiIndex index );
+PaError PaPulseAudio_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiIndex index );
 PaError PaAlsa_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiIndex index );
 PaError PaOSS_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiIndex index );
 /* Added for IRIX, Pieter, oct 2, 2003: */
@@ -93,6 +94,10 @@ PaUtilHostApiInitializer *paHostApiInitializers[] =
 
 #if PA_USE_COREAUDIO
         PaMacCore_Initialize,
+#endif
+
+#if PA_USE_PULSEAUDIO
+        PaPulseAudio_Initialize,
 #endif
 
 #if PA_USE_SKELETON
